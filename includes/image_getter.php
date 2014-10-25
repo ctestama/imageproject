b2b675702baf9ec3352ecb941a30a80a4d77977f
ctestama <colt@asu.edu>
2014-10-24 19:52:46 -0700

    blank files for imagegetter and image upload functions.  change or add these as needed

---
+++ includes/image_getter.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+
+//need to include the database configuration and mysqli object
+include('includes/config.php');
+
+
+//we need a function in here to grab images for the database and output as markup with image tags
+//will need to get the user_id variable from a post
+
+if(isset($_GET['uid']) {
+	$uid = $_GET['uid'];  //this will be the users uid, passed to this document by the url ->  localhost/includes/image_getter?uid=(user id # here)
+
+}
+
+//you'll need to pass in the mysqli database connection to any function that needs to interface with database
+
+function imageGetter($mysqli, $uid)
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ includes/image_upload.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+include('includes/config.php');
+
+//this file can include a function that uploads a user image to 
+//the image folder
+//it should check if the combination of user_id and image_path  
+//already exist in the DB, and return a message if so
+
+
+
+//and also a function to upload a user profile image
+//which will overwrite a profile photo
+
+
+
+
+
+
+
+//and also a function to overwrite a file with the same name after
+//an edit
