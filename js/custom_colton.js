2671cd4a4899a23907b31ca93d7d0c44bf6be075
ctestama <colt@asu.edu>
2014-11-08 12:21:49 -0700

    filter fix

--- js/custom_colton.js
+++ js/custom_colton.js
@@ -5,24 +5,13 @@ $( document ).ready(function() {
    $('#img_slide').slider()
     .on('slideStop', function(ev){
         var source = $('#image_pop').attr('src');
-        $('#image_pop').removeAttr('data-caman-id');
-        console.log(source);
-        var canvas = document.getElementById('image_pop');
-        var context = canvas.getContext('2d');
-        var image = new Image();
-
-        image.onload = function() {
-            $(canvas).attr({height: this.height, width: this.width, src: source});
-            context.drawImage(image, 0, 0);
-        };
-
-        image.src = source;
 
         Caman("#image_pop", function () {
-          this.brightness(ev.value).render();
+            this.brightness(ev.value).render();
         });
-
+       
         $('#slide_view').html(ev.value);
+
   });
 });
 
