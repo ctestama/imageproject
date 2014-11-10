874e10a893fb766688e7958db116cca3c046ef89
Colton Testamarck <colt@asu.edu>
2014-11-10 10:15:40 -0700

    Update bulkTest.php

--- unit_tests/bulkTest.php
+++ unit_tests/bulkTest.php
@@ -12,8 +12,7 @@ class unitTest extends PHPUnit_Framework_TestCase
 	//tests the imageGetter() function
 	function testImageGetter()
 	{
-		//This is the function that simply echos back the images.  Again I don't know what to compare
-		//or assert here so IDK what to do.
+		
 	}
 	
 	//tests the encrypt($pure_string, $encryption_key) function
@@ -36,13 +35,10 @@ class unitTest extends PHPUnit_Framework_TestCase
 	//tests the register($mysqli, $fname, $lname, $email, $password) function
 	function testRegister()
 	{
-		//havent done this one yet.  What would a failed register be?
+		
 	}
 	
 	//tests the login($mysqli, $email, $password) function
-		//this one was made for your login.  I take it you know what your email/pword is.  I do not
-		//so this should return false as it will not be your email/pword.
-		//The second part is for the correct login email/pword.  I'll need you to fill that in
 	function testLogin()
 	{	
 		$host = "localhost";
@@ -60,8 +56,8 @@ class unitTest extends PHPUnit_Framework_TestCase
 		$this->assertTrue($result == $expected);
 		
 		//test 2 - successful login; will need your help to finish
-		$email = 'YOUR CORRECT EMAIL HERE';  //add your correct email in place of the string
-		$password = 'YOUR CORRECT PASSWORD HERE';  //add your correct password in place of the string
+		$email = 'colt@asu.edu';  //add your correct email in place of the string
+		$password = 'root';  //add your correct password in place of the string
 		
 		$result = login($mysqli, $email, $password);
 		$expected = 'Success';
