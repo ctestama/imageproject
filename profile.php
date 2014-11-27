01542edd9f3543707af03e25426c1e8aa3c4ca78
ctestama <colt@asu.edu>
2014-11-26 22:26:46 -0700

    more styling

--- profile.php
+++ profile.php
@@ -2,81 +2,95 @@
 $greeting = '';
 
 if(isset($_SESSION['LOGIN_STATUS'] )&& $_SESSION['LOGIN_STATUS']) {
-	$logged_in = $_SESSION['LOGIN_STATUS'];
-	$greeting = 'Welcome ' . $_SESSION['fname'];
-	$uid = $_SESSION['uid'];
+    $logged_in = $_SESSION['LOGIN_STATUS'];
+    $greeting = 'Welcome ' . $_SESSION['fname'];
+    $uid = $_SESSION['uid'];
 } else {
-	$logged_in = FALSE;
+    $logged_in = FALSE;
 }
 
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
 
-<div class="row">
-	<div class="col-md-12" id="header">
-		The Emerald
-		<?php 
+<body>
+    <div class="row">
+        <div class="col-md-12" id="header">
+            The Emerald <?php 
+                        if($logged_in) {
+                            echo '<a id ="logout" onclick="logout()" class="btn btn-primary">Logout</a>';
+                        }   
+                    ?>
+        </div>
+    </div>
 
-			if($logged_in) {
-				echo '<a id ="logout" onclick="logout()" class="btn btn-primary">Logout</a>';
-			}	
-		?>
-	</div>
-</div>	
-<div class="container">
-	<div class="col-md-12" id="edit_container">
-		<div class="slide_container">
-			<input style="width: 250px" type="text" id="img_slide" class="span2" value="" data-slider-min="-50" 
-			data-slider-max="50" data-slider-step="1" data-slider-value="0" data-slider-orientation="horizontal" data-slider-selection="after"data-slider-tooltip="hide">
-			<span id="slide_view" class="badge"></span>
-			
-			<a class="btn btn-primary" onclick="saveBright()">Save Edited Image</a>
-			</div>
-			
-				<div id="can_wrap">
-					<canvas id="image_pop">
-					</canvas>
-				</div>
-	</div>
+    <div class="container">
+        <div class="col-md-12" id="edit_container">
+            <div class="slide_container">
+                <span>Control Image Brightness</span> <input class="span2"
+                data-slider-max="50" data-slider-min="-50"
+                data-slider-orientation="horizontal" data-slider-selection=
+                "after" data-slider-step="1" data-slider-tooltip="hide"
+                data-slider-value="0" id="img_slide" style="width: 250px" type=
+                "text" value=""> <span class="badge" id="slide_view"></span>
+                <a class="btn btn-primary" onclick="saveBright()">Save Edited
+                Image</a>
+            </div>
 
-	<div class="col-md-6">
-		<div class="row">
-	<?php 	
-		if($logged_in) {
-			echo '<a href="upload_an_image.php" id="image_button" class="btn btn-primary">Upload an image</a>';
-			if(isset($uid)) {
-				include("includes/image_getter.php"); 
-			}
-	echo '</div>';
-	echo '</div>';
-			echo '<div class="col-md-6">';
-				echo '<div class ="panel panel-success">';
-					echo '<div class="panel-heading" id="greeting">' . $greeting . '</div>';
-						echo '<div class="panel-body" id="profile">';
-							echo '<div id="profile_image_div">';
+            <div id="can_wrap">
+                <canvas id="image_pop"></canvas>
+            </div>
+        </div>
 
-							if (isset($_SESSION['profile_image'])) {
-								$profile_src = substr($_SESSION['profile_image'], 3);
-								echo '<img id="profile_img" src="'.$profile_src.'" />';
-							} 
+        <div class="col-md-6">
+            <div class="row">
+                <?php   
+                                if($logged_in) {
+                                    echo '<a href="upload_an_image.php" id="image_button" class="btn btn-primary">Upload an image</a>';
+                                    if(isset($uid)) {
+                                        include("includes/image_getter.php"); 
+                                    }
+                                }
+                            ?>
+            </div>
+        </div>
 
-							echo '</div>';
-							echo '<form action="includes/profile_image_upload.php" enctype="multipart/form-data" method="post">
-										<p>
-												 <label>Upload or change your profile image: </label>
-												<input class="btn btn-primary" type="file" name="profileimage" size="40">
-										</p>
-										<div>
-												<input class="btn btn-success" id="image_submit" type="submit" value="Upload">
-										</div>
-									</form>
-								</div>';	
-					echo '</div>';		
-			echo '</div>';
- 		} else {
- 			echo '<p>You are not logged in.</p>';
- 		}
-	?>
+        <div class="col-md-6">
+            <div class="panel panel-success">
+                <div class="panel-heading" id="greeting">
+                    <?php echo $greeting ?>
+                </div>
 
+                <div class="panel-body" id="profile">
+                    <div id="profile_image_div">
+                        <?php 
+                                                        if (isset($_SESSION['profile_image'])) {
+                                                            $profile_src = substr($_SESSION['profile_image'], 3);
+                                                            echo '<img id="profile_img" src="'.$profile_src.'" />';
+                                                        } 
+                                                    ?>
+                    </div>
 
-</div>
+                    <form action="includes/profile_image_upload.php" enctype=
+                    "multipart/form-data" method="post">
+                        <p><label>Upload or change your profile image:</label>
+                        <input class="btn btn-primary" name="profileimage"
+                        size="40" type="file"></p>
+
+                        <div>
+                            <input class="btn btn-success" id="image_submit"
+                            type="submit" value="Upload">
+                        </div>
+                    </form>
+
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
