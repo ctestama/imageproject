d14b6698c6624bbfe0db5ccb86b0327ac204c74b
ctestama <colt@asu.edu>
2014-11-26 21:30:04 -0700

    styling changes

--- index.php
+++ index.php
@@ -41,7 +41,8 @@
 
         ?>
     </div>
-    
+    <div id="footer" class="container-fluid">
+    </div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
